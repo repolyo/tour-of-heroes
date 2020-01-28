d235a27625bc672658bd3fef855ee2f5b8d3f479
Christopher Tan <chritan@lexmark.com>
2020-01-28 15:45:42 +0800

    Services
    - refactored data access to the HeroService class.
    - registered the HeroService as the provider of its service at the root level so that it can be injected anywhere in the app.
    - used Angular Dependency Injection to inject it into a component.
    - gave the HeroService get data method an asynchronous signature.
    - discovered Observable and the RxJS Observable library.
    - used RxJS of() to return an observable of mock heroes (Observable<Hero[]>).
    - component's ngOnInit lifecycle hook calls the HeroService method, not the constructor.
    - created a MessageService for loosely-coupled communication between classes.
    - The HeroService injected into a component is created with another injected service, MessageService.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,11 +13,13 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
